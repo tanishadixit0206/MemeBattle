d64f4ba3bcaafeffc4fe828a9918dcc28eb168b5
DeenankSharma <sharmadeenank@gmail.com>
2024-12-13 20:26:22 +0530

    minor changes

---
+++ frontend/game/constants.ts
@@ -0,0 +1,26 @@
+  export const joined_users = [
+    {
+        "username": "technovator",
+        "profileUrl": "https://example.com/users/technovator",
+        "votes": 0
+    },
+    {
+        "username": "codemaster23",
+        "profileUrl": "https://example.com/users/codemaster23", 
+        "votes": 0
+    },
+    {
+        "username": "digital_nomad",
+        "profileUrl": "https://example.com/users/digital_nomad",
+        "votes": 0
+    },
+    {
+        "username": "innovator_prime",
+        "profileUrl": "https://example.com/users/innovator_prime",
+        "votes": 0
+    },
+    {
+        "username": "pixel_wizard",
+        "profileUrl": "https://example.com/users/pixel_wizard",
+        "votes": 0
+    },]
--- frontend/game/src/pages/gamePage.tsx
+++ frontend/game/src/pages/gamePage.tsx
@@ -1,38 +1,14 @@
 import { Devvit, useForm, useState } from "@devvit/public-api";
 import { PlayerTile } from "../components/Playertile.js";
 import { MemeTile } from "../components/MemeTile.js";
-
+import { joined_users } from "../../constants.js";
 export const GamePage = (context: Devvit.Context): JSX.Element => {
-  const joined_users = [
-    {
-        "username": "technovator",
-        "profileUrl": "https://example.com/users/technovator",
-        "votes": 0
-    },
-    {
-        "username": "codemaster23",
-        "profileUrl": "https://example.com/users/codemaster23", 
-        "votes": 0
-    },
-    {
-        "username": "digital_nomad",
-        "profileUrl": "https://example.com/users/digital_nomad",
-        "votes": 0
-    },
-    {
-        "username": "innovator_prime",
-        "profileUrl": "https://example.com/users/innovator_prime",
-        "votes": 0
-    },
-    {
-        "username": "pixel_wizard",
-        "profileUrl": "https://example.com/users/pixel_wizard",
-        "votes": 0
-    },]
+
   const [loading, setLoading] = useState<boolean>(true)
   const [messages, setMessages] = useState<string[]>([]);
   const [votingOn, setVotingOn] = useState<boolean>(false);
-const form = useForm(
+
+  const form = useForm(
   {
     fields: [
       {
