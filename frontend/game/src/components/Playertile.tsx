c6810b684a3735b798b3827d2d3282472bf66561
DeenankSharma <sharmadeenank@gmail.com>
2024-12-13 20:23:42 +0530

    feat : adds frontend components

--- frontend/game/src/components/Playertile.tsx
+++ frontend/game/src/components/Playertile.tsx
@@ -1,9 +1,26 @@
-import { Devvit } from "@devvit/public-api";
+import { Context, Devvit,  } from "@devvit/public-api";
 
-export const PlayerTile =  (props:any):JSX.Element =>{
+
+interface User {
+  username : string;
+  url : string;
+  votes: number;
+}
+
+export const PlayerTile =  (props:User,context:Context):JSX.Element =>{
   return(<>
-    <hstack width="90%" height="12%" border="thin" borderColor="#FFB22C" cornerRadius="medium" padding="small">
-
+    <hstack alignment="middle center" width="100%" height="12%" border="thin" borderColor="#FFB22C" cornerRadius="medium" padding="medium">
+    {/* <image
+  imageWidth={400}
+  imageHeight={300}
+  width="20px"
+  height="15px"
+  url={"https://www.redditstatic.com/avatars/defaults/v2/avatar_default_6.png"}
+/>  */}
+      <text weight="bold" color="black" onPress={()=>{context.ui.navigateTo(props.url)}} >{props.username}</text>
+      <spacer width="2%"></spacer>
+      <text weight="bold" color="black">{props.votes}</text>
     </hstack>
+    <spacer width="2%"></spacer>
   </>)
 } 
